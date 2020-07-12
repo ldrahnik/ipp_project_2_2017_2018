b2d6779069602d76c95eff84b61fb8c06bcdb77b
Lukáš Drahník <ldrahnik@gmail.com>
2020-07-12 10:16:52 +0200

    automated test files finding
    Signed-off-by: Lukáš Drahník <ldrahnik@gmail.com>

--- tests/csv-supplementary-tests/_stud_tests_diff.sh
+++ tests/csv-supplementary-tests/_stud_tests_diff.sh
@@ -17,28 +17,30 @@ OUT_DIR=$4
 REF_OUT_DIR=$5
 
 # Projdeme všechny testy.
-for i in {01..18}; do
+for TEST_FILE_NAME in $(find $REF_OUT_DIR -type f -name "*.xml" -printf "%f\n" | sort -n); do
+
+    TEST_NAME=${TEST_FILE_NAME%.xml}
 
     # Zkontrolujeme návratovou hodnotu.
-    if diff "$OUT_DIR/test$i.!!!" "$REF_OUT_DIR/test$i.!!!" > /dev/null; then
+    if diff "$OUT_DIR/$TEST_NAME.!!!" "$REF_OUT_DIR/$TEST_NAME.!!!" > /dev/null; then
 
         # Pokud návratová hodnota je "0", zkontrolujeme i výstup.
-        if [[ $(head -n 1 "$REF_OUT_DIR/test$i.!!!") == "0" ]]; then
+        if [[ $(head -n 1 "$REF_OUT_DIR/$TEST_NAME.!!!") == "0" ]]; then
 
             # Porovnání výstupu s referenčním provedeme pomocí knihovny JEXAMXML.
-            eval $(java -jar $JEXAMXML_JAR_FILE "$OUT_DIR/test$i.xml" "$REF_OUT_DIR/test$i.xml" $JEXAMXML_TMP_FILE $JEXAMXML_OPTIONS_FILE > /dev/null);
+            eval $(java -jar $JEXAMXML_JAR_FILE "$OUT_DIR/$TEST_NAME.xml" "$REF_OUT_DIR/$TEST_NAME.xml" $JEXAMXML_TMP_FILE $JEXAMXML_OPTIONS_FILE > /dev/null);
             if [ $? -eq 0 ]; then
-                echo "*******TEST$i PASSED";
+                echo "*******TEST $TEST_NAME PASSED";
             # Podrobnější výpis co je rozdílné v případě neshody je uložen do souboru $JEXAMXML_TMP_FILE, který se zobrazí.
             else
                 cat $JEXAMXML_TMP_FILE
-                echo "TEST$i FAILED";
+                echo "TEST $TEST_NAME FAILED";
             fi
         # Pokud návratová hodnota není "0", program byl ukončen předčasně a výstup nebyl generován. Test proběhl úspěšně.
         else
-            echo "*******TEST$i PASSED";
+            echo "*******TEST $TEST_NAME PASSED";
         fi
     else
-        echo "TEST$i FAILED";
+        echo "TEST $TEST_NAME FAILED";
     fi
 done
